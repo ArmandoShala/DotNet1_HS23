de275aae62eec82ccaae180aa2cfe6fe81fd4973
Armando <11156050+ArmandoShala@users.noreply.github.com>
2023-09-23 00:26:59 +0200

    AB1 done, but kinda fugly

--- Arbeitsblatt1/Arbeitsblatt1/Eratosthenes.cs
+++ Arbeitsblatt1/Arbeitsblatt1/Eratosthenes.cs
@@ -1,8 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Linq;
-
 /// <summary>
 /// Sieb des Eratosthenes
 /// Autor: Karl Rege 
@@ -13,24 +8,58 @@ namespace DN1 {
 
     public class Eratosthenes  {
         
-        public PrimeType[] Sieve(int maxPrime) {
-           // TODO Implement
-           return null;
+        public PrimeType[] Sieve(int maxPrime)
+        {
+            var primes = new PrimeType[maxPrime];
+
+            for (var i = 2; i < maxPrime; i++)
+            {
+                primes[i] = isNumberPrime(i) ? PrimeType.Prime : PrimeType.NotPrime;
+            }
+            
+           return primes;
         }
+
+        private bool isNumberPrime(int number)
+        {
+            if (number <= 1) return false;
+            if (number == 2 || number == 3 || number == 5) return true;
+            if (number % 2 == 0 || number % 3 == 0 || number % 5 == 0) return false;
+
+            var boundary = (int)Math.Floor(Math.Sqrt(number));
+
+            // You can do less work by observing that at this point, all primes 
+            // other than 2 and 3 leave a remainder of either 1 or 5 when divided by 6. 
+            // The other possible remainders have been taken care of.
+            int i = 6; // start from 6, since others below have been handled.
+            while (i <= boundary)
+            {
+                if (number % (i + 1) == 0 || number % (i + 5) == 0)
+                    return false;
+
+                i += 6;
+            }
+
+            return true;
+        }
+
+        public int[] PrimesAsArray(PrimeType[] primes) => 
+            Enumerable.Range(2, primes.Length - 2).Where(i => primes[i] == PrimeType.Prime).ToArray();
         
-        public int[] PrimesAsArray(PrimeType[] primes) {
-            // TODO Implement
-            return null;
-        }
-
-        public List<int> PrimesAsList(PrimeType[] primes) {
-            // TODO Implement
-            return null;
-        }
-
-        public Dictionary<int,int>  PrimesAsDictionary(PrimeType[] primes) {
-            // TODO Implement
-            return null;
+        public List<int> PrimesAsList(PrimeType[] primes) =>
+            Enumerable.Range(2, primes.Length - 2).Where(i => primes[i] == PrimeType.Prime).ToList();
+        
+        public Dictionary<int,int> PrimesAsDictionary(PrimeType[] primes)
+        {
+            var retDict = new Dictionary<int, int>();
+            var index = 0;
+            for (var i = 2; i < primes.Length; i++)
+            {
+                if (primes[i] != PrimeType.Prime) continue;
+                retDict.Add(index, i);
+                index++;
+            }
+            return retDict;
         }
 
         public void printAll(IEnumerable<int> collection) {
