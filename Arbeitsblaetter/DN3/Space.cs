00e3fa6f445bee69a776b1f2f868646ca325729a
Armando <11156050+ArmandoShala@users.noreply.github.com>
2023-09-30 19:38:22 +0200

    started 3.2

--- Arbeitsblaetter/DN3/Space.cs
+++ Arbeitsblaetter/DN3/Space.cs
@@ -1,32 +1,40 @@
 using System;
 
-namespace DN3 {
-    public class Space {
-        
-        static void Main(string[] args) {
-            DN3.Vector omegaEarth,omegaSun, omegaGalaxy;
-            DN3.Vector rEarth, rSun, rGalaxy;
-            
-            InitOmegaVectors(out omegaEarth,out omegaSun, out omegaGalaxy);
+namespace DN3
+{
+    public class Space
+    {
+        static void Main(string[] args)
+        {
+            Vector omegaEarth, omegaSun, omegaGalaxy;
+            Vector rEarth, rSun, rGalaxy;
+
+            InitOmegaVectors(out omegaEarth, out omegaSun, out omegaGalaxy);
             InitRVectors(out rEarth, out rSun, out rGalaxy);
-            double speed = CalcSpeed(omegaEarth,omegaSun, omegaGalaxy,rEarth, rSun, rGalaxy);
-            Console.WriteLine("Speed is "+speed+" km/s");
+            var speed = CalcSpeed(omegaEarth, omegaSun, omegaGalaxy, rEarth, rSun, rGalaxy);
+            Console.WriteLine("Speed is " + speed + " km/s");
             Console.ReadLine();
         }
-        
-        public static void InitOmegaVectors(out DN3.Vector omegaEarth, out DN3.Vector omegaSun, out DN3.Vector omegaGalaxy) {
-            DN3.Vector unitVector = new DN3.Vector(0, 1, 0);
-            
-            // TODO Implement       
-        }
-        
-        public static void InitRVectors(out DN3.Vector rEarth, out DN3.Vector rSun, out DN3.Vector rGalaxy) {
-            DN3.Vector unitVector = new DN3.Vector(1, 0, 0);
-            // TODO Implement     
+
+        public static void InitOmegaVectors(out Vector omegaEarth, out Vector omegaSun, out Vector omegaGalaxy)
+        {
+            var unitVector = new Vector(0, 1, 0);
+            omegaEarth = new Vector();
+            omegaSun = new Vector();
+            omegaGalaxy = new Vector();
         }
 
-        public static double CalcSpeed(DN3.Vector omegaEarth,DN3.Vector omegaSun, DN3.Vector omegaGalaxy,DN3.Vector rEarth, DN3.Vector rSun, DN3.Vector rGalaxy) {
-            // TODO Implement     
+        public static void InitRVectors(out Vector rEarth, out Vector rSun, out Vector rGalaxy)
+        {
+            var unitVector = new Vector(1, 0, 0);
+            rEarth = new Vector(6370.0);
+            rSun = new Vector();
+            rGalaxy = new Vector();
+        }
+
+        public static double CalcSpeed(Vector omegaEarth, Vector omegaSun, Vector omegaGalaxy, Vector rEarth,
+            Vector rSun, Vector rGalaxy)
+        {
         }
     }
 }
