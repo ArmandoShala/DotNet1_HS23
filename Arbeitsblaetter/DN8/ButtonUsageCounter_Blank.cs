2f32fb7b6d2c1652746c63096b72651ff62ccb53
Armando <11156050+ArmandoShala@users.noreply.github.com>
2023-11-23 20:41:03 +0100

    added DN8 blank and other stuff idk me sleepy

---
+++ Arbeitsblaetter/DN8/ButtonUsageCounter_Blank.cs
@@ -0,0 +1,65 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Karl Rege
+ * Date: 03.08.2023
+ * Time: 15:07
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Diagnostics;
+
+namespace DN8 {
+	
+	public class ButtonUsageCounter_Blank : IDisposable  {
+        public const string CategoryName = "Button Usage";
+        public PerformanceCounter RegisterClicks;
+        public PerformanceCounter UpdateClicks;
+
+        public ButtonUsageCounter() {
+            Dispose();
+            if (!PerformanceCounterCategory.Exists(CategoryName)) {
+                
+
+                CounterCreationData UpdateButtonClick = new CounterCreationData();
+                UpdateButtonClick.CounterName = "UpdateClick";
+                UpdateButtonClick.CounterType = PerformanceCounterType.NumberOfItems32;
+                UpdateButtonClick.CounterHelp = "How many time Leave button is clicked. Useful to know how many customers left for a given time";
+                // TODO Same as above For "RegisterClick"
+   
+                
+                // Create Collection of CreationData 
+                CounterCreationDataCollection ClickCounters = new CounterCreationDataCollection();
+                ClickCounters.Add(RegButtonClick);
+                ClickCounters.Add(UpdateButtonClick);
+
+                // Create new Performance counter Category
+                PerformanceCounterCategory.Create(ButtonUsageCounter.CategoryName, "Various click counter on the Form. Used to weigh each button in this form",
+                    PerformanceCounterCategoryType.SingleInstance, ClickCounters);
+
+                // Now create actual measuring counters
+                RegisterClicks = new PerformanceCounter(CategoryName, "RegisterClick", false);
+                UpdateClicks = new PerformanceCounter(CategoryName, "UpdateClick", false);
+                // TODO: Same for "RegisterClick
+           }
+        }
+        public void Dispose() {
+            if (PerformanceCounterCategory.Exists(CategoryName)) {
+                PerformanceCounterCategory.Delete(CategoryName);
+            }
+        }
+    }
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			ButtonUsageCounter counter = new ButtonUsageCounter();
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
