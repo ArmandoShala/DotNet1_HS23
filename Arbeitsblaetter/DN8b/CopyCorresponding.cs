8f4feb8c9a7415e9fb47896fa8a07ae3e577b945
Armando <11156050+ArmandoShala@users.noreply.github.com>
2023-11-24 19:00:27 +0100

    DN9.2 done

---
+++ Arbeitsblaetter/DN8b/CopyCorresponding.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+
+namespace Copy;
+
+public class CopyCorresponding
+{
+    public void copyCorresponding(Object source, Object target)
+    {
+        if (source == null || target == null)
+            throw new ArgumentNullException("Source und Target dürfen nicht null sein.");
+
+        var sourceFields = source.GetType().GetFields(BindingFlags.Public | BindingFlags.Instance);
+        var targetFields = target.GetType().GetFields(BindingFlags.Public | BindingFlags.Instance);
+
+        foreach (var sourceField in sourceFields)
+        {
+            var targetField = Array.Find(targetFields, f => f.Name == sourceField.Name);
+
+            if (targetField != null && targetField.FieldType == sourceField.FieldType)
+                targetField.SetValue(target, sourceField.GetValue(source));
+        }
+    }
+}
