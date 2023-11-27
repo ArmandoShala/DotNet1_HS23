529163a4b6c8dee95b7b6e7b9e5dbc35ec7da0c3
Armando <11156050+ArmandoShala@users.noreply.github.com>
2023-11-27 16:32:59 +0100

    Update ReadSQLite.cs
    DN9.1 done

--- Arbeitsblaetter/DN9/ReadSQLite.cs
+++ Arbeitsblaetter/DN9/ReadSQLite.cs
@@ -1,29 +1,62 @@
+using System;
 using System.Data;
-namespace SQLiteReader;
-    class ReadSQLite
-    {
+using Microsoft.Data.Sqlite;
+using System.Collections.Generic;
 
-        public ReadSQLite()
-        {
-            IDbConnection conn = null;
-            var connectionString = @"data source=C:\Users\armandoshala\RiderProjects\DotNet1_HS23\Arbeitsblaetter\DN9\app2020.db;";
-            conn = new SqliteConnection($"Data Source={connectionString}");
-            conn.Open();
-            var command = conn.CreateCommand();
-            command.CommandText = "SELECT * FROM Appointments";
-            var reader = command.ExecuteReader();
-            var row = new object[reader.FieldCount];
-            while (reader.Read())
-            {
-                var count = reader.GetValues(row);
-                for (var i = 0; i < count; i++)
-                {
-                    Console.Write("| {0} ", row[i]);
-                    Console.WriteLine();
-                }
-            }
+namespace DN9 {
+    public class SQLiteReader {
+        public string DbConnectionString { get; set; }
+        public string TableName { get; set; }
 
-            conn.Close();
+        private IDbConnection conn = null;
+
+        public SQLiteReader(string connection, string table) {
+            DbConnectionString = connection;
+            TableName = table;
+            conn = new SqliteConnection($"Data Source={DbConnectionString}");
+			conn.Open();
         }
 
+
+        public IDbConnection DbConnection {
+            get {
+                if (conn.State != ConnectionState.Open) {
+                    conn.Open();
+                }
+                return conn;
+            }
+        }
+
+        public IEnumerable<object[]> ReadDbRow(IDbConnection conn) {
+            var command = conn.CreateCommand();
+            command.CommandText = $"SELECT * FROM {TableName}";
+            var reader = command.ExecuteReader();
+            
+            while (reader.Read()) {
+                var row = new object[reader.FieldCount];
+                reader.GetValues(row);
+                yield return row;
+            }
+        }
+        
+        public void WriteDbRow(object[] row) {
+            foreach (object o in row) {
+                Console.Write($"{o},");
+            }
+            Console.WriteLine();
+        }
+
+        public static void Main(string[] args) {
+            var connectionString = @"data source=C:\Users\armandoshala\RiderProjects\DotNet1_HS23\Arbeitsblaetter\DN9\app2020.db;";
+            var reader = new SQLiteReader(connectionString, "Appointments");
+            var conn = reader.DbConnection;
+            
+            foreach (object[] o in reader.ReadDbRow(conn)) {
+                reader.WriteDbRow(o);
+            }
+            
+            conn.Close();
+            Console.ReadKey();
+        }
     }
+}
